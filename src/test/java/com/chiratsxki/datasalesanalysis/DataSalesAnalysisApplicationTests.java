09e77c3f6dd65639923451fd5a172368ab6f2865
chiratsxki <107635322+chiratsxki@users.noreply.github.com>
2023-11-06 21:36:18 +0500

    Update DataSalesAnalysisApplicationTests.java

--- src/test/java/com/chiratsxki/datasalesanalysis/DataSalesAnalysisApplicationTests.java
+++ src/test/java/com/chiratsxki/datasalesanalysis/DataSalesAnalysisApplicationTests.java
@@ -1,13 +1,13 @@
 package com.chiratsxki.datasalesanalysis;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DataSalesAnalysisApplicationTests {
+// @SpringBootTest
+// class DataSalesAnalysisApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//     @Test
+//     void contextLoads() {
+//     }
 
-}
+// }
