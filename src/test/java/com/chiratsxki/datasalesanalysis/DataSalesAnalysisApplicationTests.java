94414c7e22a273a79900881c09da168505507ad4
Maksim Abidjanov <maximusabidjanov@gmail.com>
2023-11-06 21:45:26 +0500

    Delete method in  DataSalesAnalysisApplicationTest

--- src/test/java/com/chiratsxki/datasalesanalysis/DataSalesAnalysisApplicationTests.java
+++ src/test/java/com/chiratsxki/datasalesanalysis/DataSalesAnalysisApplicationTests.java
@@ -1,13 +1,8 @@
 package com.chiratsxki.datasalesanalysis;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DataSalesAnalysisApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
